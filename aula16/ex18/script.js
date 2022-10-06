595e9098abe849258d044a72cf6e554406044165
gustavo kim <kimyoshimura2@gmail.com>
2022-10-06 09:22:01 -0300

    CursoJavaScript

---
+++ aula16/ex18/script.js
@@ -0,0 +1,16 @@
+let num = document.querySelector('input#fum')
+let lista = document.querySelector('selec#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >=1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, ) {
+
+}
